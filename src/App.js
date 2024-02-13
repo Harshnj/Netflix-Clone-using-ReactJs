2fb4f4c33fe01fd983846fc4e4f0306d56ae869f
Harshnj <95063086+Harshnj@users.noreply.github.com>
2024-02-14 00:01:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import './App.scss';
 import Header from './components/Header.js';
 import HomeBanner from "./components/HomeBanner";
-import Login from "./components/Login";
+// import Login from "./components/Login";
 import Banner from "./components/Banner";
 import List from "./components/List";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -18,18 +18,18 @@ function App() {
               <HomeBanner/>
             </React.Fragment>
           }/>
-          <Route path="/login" element={
-            <React.Fragment>
-              <Header/>
-              <Login/>
-            </React.Fragment>
-          }/>
-          <Route path="/register" element={
-            <React.Fragment>
-              <Header/>
-              <Login/>
-            </React.Fragment>
-          }/>
+          // <Route path="/login" element={
+          //   <React.Fragment>
+          //     <Header/>
+          //     <Login/>
+          //   </React.Fragment>
+          // }/>
+          // <Route path="/register" element={
+          //   <React.Fragment>
+          //     <Header/>
+          //     <Login/>
+          //   </React.Fragment>
+          // }/>
           <Route path="/dashboard" element={
             <React.Fragment>
               <Header/>
